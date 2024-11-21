7f22e58b955eab9a8fc0eb065980ae7b00287af4
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-11-21 19:24:52 +0530

    somechange

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express=require('express');
-const mainRouter=require('./routes/index');
-const cors=require('cors');
-const app=express();
+const express = require('express');
+const mainRouter = require('./routes/index');
+const cors = require('cors');
+
+const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1",mainRouter);
-app.listen(3000)
+app.use("/api/v1", mainRouter);
+
+// Export the app for Vercel
+module.exports = app;
