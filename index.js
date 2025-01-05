5b7ee775a3e97269271a7629c9846674e6911999
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2025-01-06 01:06:42 +0530

    trying to fix erros

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/',(req,res)=>{
+    res.send("welcome to vercel")
+})
 app.use("/api/v1", mainRouter);
 
 // Export the app for Vercel
