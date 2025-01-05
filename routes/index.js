a7288bdffd2d8a0562b5779c44260349dd7569ba
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2025-01-06 01:18:21 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express=require('express')
 const router=express.Router()
 const userRouter=require('./user')
 const accountRouter=require('./account')
+router.use('/',(req,res)=>{
+    res.send("welcome to vercel")
+})
 router.use("/user",userRouter)
 router.use("/account",accountRouter)
 module.exports=router;
